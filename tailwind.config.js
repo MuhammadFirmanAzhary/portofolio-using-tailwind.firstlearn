ae193aa910e9cc2556780be28f4deb3d6b35ad87
muhammadfirmanazhary <muhammadfirmanazhary.com>
2022-09-23 20:28:57 +0700

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container:{
+      center: true,
+      padding: "16px",
+    },
+    extend: {},
+  },
+  screens: {
+    '2xl':'1320px',
+  },
+  plugins: [],
+}
